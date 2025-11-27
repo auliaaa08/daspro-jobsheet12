67e3d25c2f9823a194cc319ab88bc6c7c0d00241
auliaaa08 <ab.auliaabilqis@gmail.com>
2025-11-27 10:31:22 +0700

    Percobaan 4: Modifikasi pada fungsi varargs

--- PengunjungCafe06.java
+++ PengunjungCafe06.java
@@ -2,8 +2,8 @@ public class PengunjungCafe06 {
 
     public static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar nama pengunjung: ");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String nama: namaPengunjung) {
+            System.out.println("- " + nama);
         }
     }
     public static void main(String[] args) {
